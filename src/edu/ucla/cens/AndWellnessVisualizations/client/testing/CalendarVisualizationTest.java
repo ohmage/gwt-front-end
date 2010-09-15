ff892916408a8b853063cc542129c5064194608d
John Hicks <johnhicks@gmail.com>
2010-09-14 17:01:02 -0700

    Added a testing package for testing code

---
+++ src/edu/ucla/cens/AndWellnessVisualizations/client/testing/CalendarVisualizationTest.java
@@ -0,0 +1,27 @@
+package edu.ucla.cens.AndWellnessVisualizations.client.testing;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import edu.ucla.cens.AndWellnessVisualizations.client.view.CalendarVisualizationView;
+import edu.ucla.cens.AndWellnessVisualizations.client.view.CalendarVisualizationViewImpl;
+
+/**
+ * Test of the CalendarVisualization.  For now only initializes the View and attaches
+ * it to the test_calendar_visualization div.
+ * 
+ * @author jhicks
+ *
+ */
+public class CalendarVisualizationTest implements EntryPoint {
+
+    /**
+     * Creates the ValendarVisualizationView on the RootPanel
+     */
+    public void onModuleLoad() {
+        CalendarVisualizationView<Object> calViz = new CalendarVisualizationViewImpl<Object>();
+        
+        RootPanel.get("test_calendar_visualization").add(calViz.asWidget());
+    }
+
+}
