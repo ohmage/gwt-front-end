8007e54d38288b2171a2d7fb5a283c5b342f004a
John Hicks <johnhicks@gmail.com>
2010-09-21 18:06:13 -0700

    Added a ClientInfo that holds various info about the client (client name)

---
+++ src/edu/ucla/cens/AndWellnessVisualizations/client/ClientInfo.java
@@ -0,0 +1,15 @@
+package edu.ucla.cens.AndWellnessVisualizations.client;
+
+/**
+ * Holds static information about the client.
+ * 
+ * @author jhicks
+ *
+ */
+public class ClientInfo {
+    private final static String clientName = "AndWellnessFrontend";
+    
+    public static String getClientName() {
+        return clientName;
+    }
+}
