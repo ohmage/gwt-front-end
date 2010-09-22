fc39a0534927e335db9122ab1445c096e46602a9
John Hicks <johnhicks@gmail.com>
2010-09-21 18:06:43 -0700

    Added a Model to hold the campaign information obtained from the config API

---
+++ src/edu/ucla/cens/AndWellnessVisualizations/client/model/CampaignInfo.java
@@ -0,0 +1,16 @@
+package edu.ucla.cens.AndWellnessVisualizations.client.model;
+
+/**
+ * Stores information about the current AndWellness campaign.
+ * 
+ * @author jhicks
+ *
+ */
+public class CampaignInfo {
+
+    public String getCampaignId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
