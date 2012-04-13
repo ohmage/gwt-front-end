f7503a5e5b40608d072c15de7a9287ab2922a669
Eric Wang <ewang9@gmail.com>
2012-04-13 03:09:08 -0700

    Fixed enter key not submitting form on login UI in Firefox (issue #245)

--- src/edu/ucla/cens/mobilize/client/view/LoginViewAndWellnessImpl.java
+++ src/edu/ucla/cens/mobilize/client/view/LoginViewAndWellnessImpl.java
@@ -7,6 +7,8 @@ import com.google.gwt.dom.client.UListElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -25,125 +27,124 @@ import edu.ucla.cens.mobilize.client.ui.WaitIndicator;
 
 
 public class LoginViewAndWellnessImpl extends Composite implements LoginView {
-    @UiTemplate("LoginViewAndWellness.ui.xml")
-    interface LoginBoxViewUiBinder extends UiBinder<Widget, LoginViewAndWellnessImpl> {}
-    private static LoginBoxViewUiBinder uiBinder =
-      GWT.create(LoginBoxViewUiBinder.class);
-    
-    // Fields defined in the ui XML
-    @UiField TextBox userNameTextBox;
-    @UiField PasswordTextBox passwordTextBox;
-    @UiField Button loginButton;
-    
-    private Presenter presenter;
-    
-    public LoginViewAndWellnessImpl() {
-        initWidget(uiBinder.createAndBindUi(this)); 
-        initEventHandlers();
-        
-        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-            public void execute () {
-                userNameTextBox.setFocus(true);
-            }
-        });
-    }
-    
-    private void initEventHandlers() {
-      // clicking login button submits (note: pressing enter
-      //  while login has focus also submits)
-      loginButton.addClickHandler(new ClickHandler() {
-        @Override
-        public void onClick(ClickEvent event) {
-          submit();
-        }
-      });
-      
-      // pressing enter in user name box submits
-      userNameTextBox.addKeyPressHandler(new KeyPressHandler() {
-        @Override
-        public void onKeyPress(KeyPressEvent event) {
-          if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-            submit();
-          }
-        }
-      });
-      
-      // pressing enter in password box submits
-      passwordTextBox.addKeyPressHandler(new KeyPressHandler() {
-        @Override
-        public void onKeyPress(KeyPressEvent event) {
-          if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-            submit();
-          }
-        }
-      });
-    }
-   
-    // verifies that both name and password are set then notifies presenter
-    private void submit() {
-      this.presenter.onSubmit();
-    }
-    
-    /**
-     * Sets the views presenter to call to handle events.
-     * 
-     * @param presenter The presenter to bind to.
-     */
-    public void setPresenter(Presenter presenter) {
-        this.presenter = presenter;
-    }
+	@UiTemplate("LoginViewAndWellness.ui.xml")
+	interface LoginBoxViewUiBinder extends UiBinder<Widget, LoginViewAndWellnessImpl> {}
+	private static LoginBoxViewUiBinder uiBinder =
+			GWT.create(LoginBoxViewUiBinder.class);
 
-    @Override
-    public void setLoginFailed(String msg) {
-      ErrorDialog.show("Invalid username or password", msg);
-    }
+	// Fields defined in the ui XML
+	@UiField TextBox userNameTextBox;
+	@UiField PasswordTextBox passwordTextBox;
+	@UiField Button loginButton;
 
-    @Override
-    public String getUsername() {
-      return userNameTextBox.getText();
-    }
+	private Presenter presenter;
 
-    @Override
-    public String getPassword() {
-      return passwordTextBox.getText();
-    }
+	public LoginViewAndWellnessImpl() {
+		initWidget(uiBinder.createAndBindUi(this)); 
+		initEventHandlers();
 
-    @Override
-    public void disableLoginForm() {
-      loginButton.setEnabled(false);
-      userNameTextBox.setEnabled(false);
-      passwordTextBox.setEnabled(false);
-      WaitIndicator.show();
-    }
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			public void execute () {
+				userNameTextBox.setFocus(true);
+			}
+		});
+	}
 
-    @Override
-    public void enableLoginForm() {
-      loginButton.setEnabled(true);
-      userNameTextBox.setEnabled(true);
-      passwordTextBox.setEnabled(true);
-      WaitIndicator.hide();
-    }
+	private void initEventHandlers() {
+		// clicking login button submits (note: pressing enter while login has focus also submits)
+		loginButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				submit();
+			}
+		});
 
-    @Override
-    public void showError(String errorMsg) {
-      ErrorDialog.show(errorMsg);
-    }
+		// pressing enter in user name box submits
+		userNameTextBox.addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					submit();
+				}
+			}
+		});
+
+		// pressing enter in password box submits
+		passwordTextBox.addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					submit();
+				}
+			}
+		});
+	}
+
+	// verifies that both name and password are set then notifies presenter
+	private void submit() {
+		this.presenter.onSubmit();
+	}
+
+	/**
+	 * Sets the views presenter to call to handle events.
+	 * 
+	 * @param presenter The presenter to bind to.
+	 */
+	public void setPresenter(Presenter presenter) {
+		this.presenter = presenter;
+	}
+
+	@Override
+	public void setLoginFailed(String msg) {
+		ErrorDialog.show("Invalid username or password", msg);
+	}
+
+	@Override
+	public String getUsername() {
+		return userNameTextBox.getText();
+	}
+
+	@Override
+	public String getPassword() {
+		return passwordTextBox.getText();
+	}
+
+	@Override
+	public void disableLoginForm() {
+		loginButton.setEnabled(false);
+		userNameTextBox.setEnabled(false);
+		passwordTextBox.setEnabled(false);
+		WaitIndicator.show();
+	}
+
+	@Override
+	public void enableLoginForm() {
+		loginButton.setEnabled(true);
+		userNameTextBox.setEnabled(true);
+		passwordTextBox.setEnabled(true);
+		WaitIndicator.hide();
+	}
+
+	@Override
+	public void showError(String errorMsg) {
+		ErrorDialog.show(errorMsg);
+	}
 
 	@Override
 	public void setNotificationMessage(String message) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setSelfRegistrationEnabled(boolean isEnabled) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setLoginRecoveryEnabled(boolean isEnabled) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
--- src/edu/ucla/cens/mobilize/client/view/LoginViewOhmageImpl.java
+++ src/edu/ucla/cens/mobilize/client/view/LoginViewOhmageImpl.java
@@ -6,6 +6,8 @@ import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -25,119 +27,119 @@ import edu.ucla.cens.mobilize.client.ui.WaitIndicator;
 
 
 public class LoginViewOhmageImpl extends Composite implements LoginView {
-    @UiTemplate("LoginViewOhmage.ui.xml")
-    interface LoginBoxViewUiBinder extends UiBinder<Widget, LoginViewOhmageImpl> {}
-    private static LoginBoxViewUiBinder uiBinder = GWT.create(LoginBoxViewUiBinder.class);
-    
-    // Fields defined in the ui XML
-    @UiField TextBox userNameTextBox;
-    @UiField PasswordTextBox passwordTextBox;
-    @UiField Button loginButton;
-    @UiField HTMLPanel selfRegistrationText;
-    @UiField HTMLPanel loginRecoveryText;
-    @UiField HTMLPanel messagePanel;
-    @UiField HTMLPanel messageBox;
-    @UiField InlineHyperlink selfRegisterLink;
-    
-    private Presenter presenter;
-    
-    public LoginViewOhmageImpl() {
-        initWidget(uiBinder.createAndBindUi(this)); 
-        initEventHandlers();
-        
-        // Defaults
-        setNotificationMessage(null);
-        setSelfRegistrationEnabled(false);
-        setLoginRecoveryEnabled(true);
-        
-        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-            public void execute () {
-                userNameTextBox.setFocus(true);
-            }
-        });
-    }
-    
-    @SuppressWarnings("deprecation")
+	@UiTemplate("LoginViewOhmage.ui.xml")
+	interface LoginBoxViewUiBinder extends UiBinder<Widget, LoginViewOhmageImpl> {}
+	private static LoginBoxViewUiBinder uiBinder = GWT.create(LoginBoxViewUiBinder.class);
+
+	// Fields defined in the ui XML
+	@UiField TextBox userNameTextBox;
+	@UiField PasswordTextBox passwordTextBox;
+	@UiField Button loginButton;
+	@UiField HTMLPanel selfRegistrationText;
+	@UiField HTMLPanel loginRecoveryText;
+	@UiField HTMLPanel messagePanel;
+	@UiField HTMLPanel messageBox;
+	@UiField InlineHyperlink selfRegisterLink;
+
+	private Presenter presenter;
+
+	public LoginViewOhmageImpl() {
+		initWidget(uiBinder.createAndBindUi(this)); 
+		initEventHandlers();
+
+		// Defaults
+		setNotificationMessage(null);
+		setSelfRegistrationEnabled(false);
+		setLoginRecoveryEnabled(true);
+
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			public void execute () {
+				userNameTextBox.setFocus(true);
+			}
+		});
+	}
+
+	@SuppressWarnings("deprecation")
 	private void initEventHandlers() {
-      // clicking login button submits (note: pressing enter
-      //  while login has focus also submits)
-      loginButton.addClickHandler(new ClickHandler() {
-        @Override
-        public void onClick(ClickEvent event) {
-          submit();
-        }
-      });
-      
-      // pressing enter in user name box submits
-      userNameTextBox.addKeyPressHandler(new KeyPressHandler() {
-        @Override
-        public void onKeyPress(KeyPressEvent event) {
-          if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-            submit();
-          }
-        }
-      });
-      
-      // pressing enter in password box submits
-      passwordTextBox.addKeyPressHandler(new KeyPressHandler() {
-        @Override
-        public void onKeyPress(KeyPressEvent event) {
-          if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-            submit();
-          }
-        }
-      });
-    }
-   
-    // verifies that both name and password are set then notifies presenter
-    private void submit() {
-      this.presenter.onSubmit();
-    }
-    
-    /**
-     * Sets the views presenter to call to handle events.
-     * 
-     * @param presenter The presenter to bind to.
-     */
-    public void setPresenter(Presenter presenter) {
-        this.presenter = presenter;
-    }
+		// clicking login button submits (note: pressing enter
+		//  while login has focus also submits)
+		loginButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				submit();
+			}
+		});
 
-    @Override
-    public void setLoginFailed(String msg) {
-      ErrorDialog.show("The login information you entered is incorrect", msg);
-    }
+		// pressing enter in user name box submits
+		userNameTextBox.addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					submit();
+				}
+			}
+		});
 
-    @Override
-    public String getUsername() {
-      return userNameTextBox.getText();
-    }
+		// pressing enter in password box submits
+		passwordTextBox.addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					submit();
+				}
+			}
+		});
+	}
 
-    @Override
-    public String getPassword() {
-      return passwordTextBox.getText();
-    }
+	// verifies that both name and password are set then notifies presenter
+	private void submit() {
+		this.presenter.onSubmit();
+	}
 
-    @Override
-    public void disableLoginForm() {
-      loginButton.setEnabled(false);
-      userNameTextBox.setEnabled(false);
-      passwordTextBox.setEnabled(false);
-      WaitIndicator.show();
-    }
+	/**
+	 * Sets the views presenter to call to handle events.
+	 * 
+	 * @param presenter The presenter to bind to.
+	 */
+	public void setPresenter(Presenter presenter) {
+		this.presenter = presenter;
+	}
 
-    @Override
-    public void enableLoginForm() {
-      loginButton.setEnabled(true);
-      userNameTextBox.setEnabled(true);
-      passwordTextBox.setEnabled(true);
-      WaitIndicator.hide();
-    }
+	@Override
+	public void setLoginFailed(String msg) {
+		ErrorDialog.show("The login information you entered is incorrect", msg);
+	}
 
-    @Override
-    public void showError(String errorMsg) {
-      ErrorDialog.show(errorMsg);
-    }
+	@Override
+	public String getUsername() {
+		return userNameTextBox.getText();
+	}
+
+	@Override
+	public String getPassword() {
+		return passwordTextBox.getText();
+	}
+
+	@Override
+	public void disableLoginForm() {
+		loginButton.setEnabled(false);
+		userNameTextBox.setEnabled(false);
+		passwordTextBox.setEnabled(false);
+		WaitIndicator.show();
+	}
+
+	@Override
+	public void enableLoginForm() {
+		loginButton.setEnabled(true);
+		userNameTextBox.setEnabled(true);
+		passwordTextBox.setEnabled(true);
+		WaitIndicator.hide();
+	}
+
+	@Override
+	public void showError(String errorMsg) {
+		ErrorDialog.show(errorMsg);
+	}
 
 	@Override
 	public void setNotificationMessage(String message) {
@@ -145,7 +147,7 @@ public class LoginViewOhmageImpl extends Composite implements LoginView {
 			messagePanel.setVisible(false);
 			return;
 		}
-		
+
 		messageBox.clear();
 		messageBox.add(new InlineLabel(message));
 		messagePanel.setVisible(true);
@@ -155,7 +157,7 @@ public class LoginViewOhmageImpl extends Composite implements LoginView {
 	public void setSelfRegistrationEnabled(boolean isEnabled) {
 		selfRegistrationText.setVisible(isEnabled);
 	}
-	
+
 	@Override
 	public void setLoginRecoveryEnabled(boolean isEnabled) {
 		loginRecoveryText.setVisible(isEnabled);
